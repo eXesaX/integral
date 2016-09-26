46d7422c2baf14ffd357267a7dd77fc5d803a949
exesax <exesa@yandex.ru>
2016-09-26 18:33:21 +0500

    fix teylor

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import math
 
 print("Введите а: ")
-a = float(input())
+a = int(input())
 print("Введите b: ")
-b = float(input())
+b = int(input())
 print("Введите n: ")
 n = int(input())
 
 interval = (b - a) / n
-c = (b - a) / 2
+c = (b + a) / 2
 
 square_integral_left = 0
 square_integral_right = 0
 square_integral_mid = 0
 teylor = math.sin(c)
 
-
-
-for i in range(n):
+for i in range(a, b):
     square_integral_left += math.sin(i * interval) * ((i + 1) * interval - i * interval)
     square_integral_right += math.sin((i + 1) * interval) * ((i + 1) * interval - i * interval)
     square_integral_mid += math.sin(((i + 1) * interval + i * interval) / 2) * ((i + 1) * interval - i * interval)
-    teylor += math.sin(c + math.pi*i/n) * (b - c) ** (n + 1) / (math.factorial(n) * (n + 1)) - \
-              math.sin(c + math.pi*i/n) * (a - c) ** (n + 1) / (math.factorial(n) * (n + 1))
+
+for i in range(1, n + 1):
+    teylor += math.sin(c + math.pi*i/2) * ((b - c) ** (i + 1)) / (math.factorial(i + 1))
+    teylor -= math.sin(c + math.pi*i/2) * ((a - c) ** (i + 1)) / (math.factorial(i + 1))
+
 
 
 print("Прямоугольники: ")
